5430e934c939797bd9fcbe22df0f6b7abe6a4bd3
fjfoxru <61179622+fjfoxru@users.noreply.github.com>
2021-01-18 12:28:45 +0300

    Update character.js

--- src/js/character.js
+++ src/js/character.js
@@ -1,20 +1,24 @@
 export default class Character {
-  constructor(name) {
+  constructor(name, type) {
     try {
       if (name.length < 2 || name.length > 10) {
         throw new Error('Имя неправильное');
       }
+      if (type !== 'Bowman' && type !== 'Swordsman' && type !== 'Magician' && type !== 'Daemon' && type !== 'Undead' && type !== 'Zombie') {
+        throw new Error('Тип неправильный');
+      }
       this.name = name;
       this.health = 100;
       this.level = 1;
+      this.type = type;
     } catch (error) {
-      console.log(error);
+      throw error;
     }
   }
 
   levelUp() {
     try {
-      if (this.health == 0) {
+      if (this.health <= 0) {
         throw new Error('Жизнь = 0');
       }
       this.level += 1;
@@ -22,12 +26,11 @@ export default class Character {
       this.defence += this.defence / 100 * 20;
       this.health = 100;
     } catch (error) {
-      console.log(error);
+      throw error;
     }
   }
   damage(points) {
-      if (this.health >= 0) {
-        this.health -= points * (1 - this.defence / 100);
-      }
+    if (this.health <= 0) return false;
+      this.health -= points * (1 - this.defence / 100);
   }
 }
